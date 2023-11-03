3e558b5efd45bc0ab8b57b062a530f7b31e5c5ad
dancrump1 <danielcrump@outlook.com>
2023-11-03 11:25:03 -0400

    Load css before js

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,22 @@
-import liveReload from 'vite-plugin-live-reload'
-import legacy from '@vitejs/plugin-legacy'
-import critical from 'rollup-plugin-critical'
-import viteCompression from 'vite-plugin-compression'
+import liveReload from "vite-plugin-live-reload";
+import viteCompression from "vite-plugin-compression";
 
 export default ({ command }) => ({
-	base: command === 'serve' ? '' : '/dist/',
-	css: { preprocessorOptions: { scss: { charset: false } } },
-	build: {
-		manifest: true,
-		outDir: './web/dist/',
-		rollupOptions: {
-			input: {
-				app: './src/js/app.ts',
-			},
-			output: {
-				sourcemap: true,
-			},
-		},
-	},
-	server: {
-		host: '0.0.0.0',
-		port: 3000,
-        strictPort: true,
-	},
-	plugins: [
-		liveReload(['./templates/**/*', './vendor/szenario/**/*']),
-		legacy({
-			targets: ['defaults'],
-			additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
-		}),
-		critical({
-			criticalUrl: 'http://localhost',
-			criticalBase: './web/dist/criticalcss/',
-			criticalPages: [{ uri: '/', template: 'index' }],
-			criticalConfig: {},
-		}),
-		viteCompression(),
-	],
-})
+  base: command === "serve" ? "" : "/dist/",
+  build: {
+    emptyOutDir: true,
+    manifest: true,
+    outDir: "./web/dist/",
+    sourcemap: true,
+    rollupOptions: {
+      input: {  css: "src/scss/main.scss", app: "/src/js/app.ts" },
+      output: { sourcemap: true },
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    strictPort: true,
+  },
+  plugins: [liveReload(["./templates/**/*"]), viteCompression()],
+});
