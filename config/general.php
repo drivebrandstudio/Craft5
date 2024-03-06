df76e29e7decda8683a6b8c8b940ecd0815274ce
Dan <danielcrump@outlook.com>
2024-03-06 13:19:56 -0500

    make general.php better

--- config/general.php
+++ config/general.php
@@ -11,19 +11,22 @@
 use craft\config\GeneralConfig;
 use craft\helpers\App;
 
+$isDev = App::env('CRAFT_ENVIRONMENT') == 'dev';
+$isProd = App::env('CRAFT_ENVIRONMENT') == 'production';
+
 return GeneralConfig::create()
     // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
     ->defaultWeekStartDay(0)
     // Prevent generated URLs from including "index.php"
     ->omitScriptNameInUrls()
     // Enable Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
-    ->devMode(App::env('CRAFT_ENVIRONMENT') ?? false)
+    ->devMode($isDev)
     // Preload Single entries as Twig variables
     ->preloadSingles()
     // Allow administrative changes
-    ->allowAdminChanges(App::env('ALLOW_ADMIN_CHANGES') ?? false)
+    ->allowAdminChanges($isDev)
     // Disallow robots
-    ->disallowRobots(App::env('DISALLOW_ROBOTS') ?? false)
+    ->disallowRobots(!$isProd)
     // Prevent user enumeration attacks
     ->preventUserEnumeration()
     // Set the @webroot alias so the clear-caches command knows where to find CP resources
